0b9d95f4451b71787b7885377042e77d67dc2556
Sovan Roy <roysovan00@gmail.com>
2024-01-01 11:43:24 +0530

    updated index.html and js file

--- script.js
+++ script.js
@@ -65,24 +65,6 @@ const elems2 = gsap.utils.toArray(".elem2");
 const loop2 = horizontalLoop(elems2, { paused: false, repeat: -1, reversed: true });
 
 
-//slides
-var slideIndex = 1;
-showDivs(slideIndex);
-
-function plusDivs(n) {
-  showDivs(slideIndex += n);
-}
-
-function showDivs(n) {
-  var i;
-  var x = document.getElementsByClassName("mySlides");
-  if (n > x.length) { slideIndex = 1 }
-  if (n < 1) { slideIndex = x.length }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[slideIndex - 1].style.display = "flex";
-}
 
 //faq
 
